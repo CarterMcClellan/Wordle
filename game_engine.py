b41f57ed9e94e26ded13ab6e59212aa4d3de2d72
CarterMcClellan <carter.s.mcclellan@gmail.com>
2022-04-23 11:50:50 -0700

    remove answers

--- game_engine.py
+++ game_engine.py
@@ -1,145 +1,195 @@
+from __future__ import annotations
+
 import os, sys, random, time
-from typing import List, Union
 
 from rich.console import Console
 
 class Wordle:
-
-    def __init__(self, answer: str, word_bank: List[str], ai=False):
-        # TODO write some more input handlers
+    def __init__(
+        self,
+        answer: str,
+        word_bank: list[str],
+        ai: bool = False,
+        show_answer: bool = True,
+    ):
+        # check that a list is passed
         if word_bank is None:
             raise Exception("You must provide a valid Word Bank")
         
+        # check that the len of all words in the word bank are the same
+        expected_len = len(word_bank[0])
+        for word in word_bank:
+            if len(word) != expected_len:
+                raise Exception(
+                    f"""Expected all words in word bank to be of len 
+                    {expected_len}, instead found {word} (len= {len(word)}"
+                    """
+                )
+
         self.word_bank = word_bank
+        
+        # Validate chosen answer.
+        # If an answer is not provided then randomly select
+        # an answer from the word bank
         if answer is None:
             self.answer = random.choice(word_bank)
         else:
             self.answer = answer
 
-        print(f"The actual answer is {self.answer} ssssh, dont tell anyone")
-
+            if self.answer not in self.word_bank:
+                raise Exception(
+                    f"""Expected user to pass word in word bank.
+                    {self.answer} does not exist in the word bank.
+                    """
+                )
+        
+        # Make it optional for users to see the answer
+        if show_answer:
+            print(f"The actual answer is {self.answer} ssssh, dont tell anyone")
+        
+        # List of all past guesses
         self.past_guesses = []
-        self.board = []
-
+        
+        # Boolean to indicate whether a human is playing or an ai
+        # is playing. If an a.i is playing, then we render the output
+        # slightly differently
+        self.ai = ai
+        
+        # Object from 'Rich' python library, which we use to render
+        # the board
         self.board_printer = Console()
 
-        self.ai = ai
-    
-    def get_word_bank(self):
-        return self.word_bank
-
-    def get_past_guesses(self):
-        return self.past_guesses
-    
-    def guess(self):
+    def guess(self) -> None:
+        """ method which prompts user to make a guess """
         valid = False
         while not valid:
             user_guess = input("What is your guess: ")
             user_guess = user_guess.upper()
             if user_guess not in self.word_bank:
-                print(f"""
+                print(
+                    f"""
                 Oops! Looks like your guess: {guess}, was not found
                 in our Word Bank of valid guesses. Please guess again
-                """)
-            elif len(user_guess) != len(self.answer):
-                print(f"""
-                Oops! Looks like your guess: {guess}, was not the right 
-                length. Expected: {len(self.answer)} Found: {len(guess)}
-                """)
+                """
+                )
             else:
                 valid = True
 
         evaluate_guess(user_guess)
+
+    def get_word_bank(self) -> list[str]:
+        """ method which returns word bank """
+
+        raise NotImplementedError("you need to implement this method")
+
+    def get_past_guesses(self) -> list[tuple(str)]
+        """ method which returns past guesses"""
+
+        raise NotImplementedError("you need to implement this method")
+
+    def evaluate_guess(self, user_guess) -> None:
+        """
+        method which evaluates the users guess.
+        should do 2 things
+
+        1) determine which letters are grey, yellow, and green
+        2) append those letters (in order) as a list of tuples to past guesses
+
+        Edge Cases to think about:
         
-    def evaluate_guess(self, user_guess) -> List[Union[str, str]]:
-        green_tiles = self.check_for_green_tiles(user_guess)
-        yellow_tiles = self.check_for_yellow_tiles(user_guess)
+        - Suppose the answer is "STUCK" and your guess is "OASIS".
+          The first "S" should be yellow but the second "S" should be
+          grey (to indicate that there is only 1 "S" in the answer)
 
-        result = green_tiles + yellow_tiles
-        result.sort(key=lambda x: x[2])
+        - Similarly suppose the answer is "CRANE" and your guess is 
+          "CROCK" the first "C" should be green and the second should be
+          grey.
+
+        Why are these edge cases important?
         
-        # which indices did we miss?
-        # these should be marked "grey"
-        seen = set()
-        grey_tiles = []
-        for (_, _, idx) in result:
-            seen.add(idx)
+        - It means your functions "check_for_grey_tiles", "check_for_green_tiles"
+          and "check_for_yellow_tiles" are not independent! If the letter appears
+          in the answer once but the guess twice, and you have already marked it 
+          yellow once, then your list of grey tiles must include it. (see example
+          above).
+        """
 
-        for i in range(len(user_guess)):
-            if i not in seen:
-                grey_tiles.append(("grey", user_guess[i], i))
-
-        result = result + grey_tiles
-        result.sort(key=lambda x: x[2])
-
-        self.past_guesses.append(result)
+        raise NotImplementedError("you need to implement this method")
     
-    def check_for_yellow_tiles(self, user_guess: str):
-        from collections import Counter
-        answer_letters = Counter(self.answer)
-        yellow_tiles = []
-        for idx in range(len(user_guess)):
-            letter = user_guess[idx]
+    def check_for_grey_tiles(self, user_guess: str) -> list[tuple(str, str, int)]:
+        """
+        method which returns all letters in the user guess which are
+        NOT in the word!
 
-            # yellow tiles cannot be green
-            # so skip all tiles which meet 
-            # the green condition
-            if self.answer[idx] == letter: 
-                # 2) decrement counts for green tiles
-                answer_letters[letter] -= 1
-                continue
-            
-            # 1) make sure that we do not double count
-            elif letter in answer_letters and answer_letters[letter] > 0:
-                answer_letters[letter] -= 1
-                yellow_tiles.append(("yellow", letter, idx)) 
+        note:
+            you must return a list of tuples. each tuple must be exactly 3 
+            elements long. (color, letter, idx), eg. ("grey", "A", 0)
+        """
 
-        return yellow_tiles
+        raise NotImplementedError("you need to implement this method")
 
-    def check_for_green_tiles(self, user_guess: str):
-        green_tiles = []
-        for idx in range(len(user_guess)):
-            if self.answer[idx] == user_guess[idx]:
-                green_tiles.append(("green", user_guess[idx], idx))
+    def check_for_yellow_tiles(self, user_guess: str) -> list[tuple(str, str, int)]:
+        """
+        method which returns all letters in the user's guess which are
+        in the word but are in a DIFFERENT position
 
-        return green_tiles
+        note:
+            you must return a list of tuples. each tuple must be exactly 3 
+            elements long. (color, letter, idx), eg. ("yellow", "A", 0)
+        """
 
-    def render_result(self):
-       os.system("clear") 
-       if self.ai:
-           print(f"You are watching an a.i play")
-           print(f"The word it is trying to guess is {self.answer}")
+        raise NotImplementedError("you need to implement this method")
 
-       # paint the rows which have already
-       # been guessed 
-       for guess_idx, guess in enumerate(self.past_guesses):
-           if guess_idx == len(self.past_guesses) - 1:
-               delay = .2
-           else:
-               delay = 0
+    def check_for_green_tiles(self, user_guess: str) -> list[tuple(str, str, int)]:
+        """
+        method which returns all letters in the user guess which are
+        in the word and are in the SAME position
 
-           for (color, letter, idx) in guess:
-               time.sleep(delay)
-               if idx == len(self.answer) - 1:
-                   end = ""
-               else:
+        note:
+            you must return a list of tuples. each tuple must be exactly 3 
+            elements long. (color, letter, idx), eg. ("green", "A", 0)
+        """
+
+        raise NotImplementedError("you need to implement this method")
+
+    def render_result(self) -> None:
+        """Method which renders the current wordle board (ignore this)"""
+
+        os.system("clear")
+        if self.ai:
+            print(f"You are watching an a.i play")
+            print(f"The word it is trying to guess is {self.answer}")
+
+        for guess_idx, guess in enumerate(self.past_guesses):
+            if guess_idx == len(self.past_guesses) - 1:
+                delay = 0.2
+            else:
+                delay = 0
+
+            for (color, letter, idx) in guess:
+                time.sleep(delay)
+                if idx == len(self.answer) - 1:
+                    end = ""
+                else:
                     end = " "
 
-               if color == "yellow":
-                  self.board_printer.print(letter, style="bold yellow", end=end)
-               elif color == "grey":
-                   self.board_printer.print(letter, style="bold #808080", end=end)
-               elif color == "green":
-                   self.board_printer.print(letter, style="bold green", end=end)
-               else:
-                   raise Exception(f"Ooops, looks like you did not pass a valid color. \
-                                     Should be: [yellow, gray, green] instead found: {color}")
-           print()
+                if color == "yellow":
+                    self.board_printer.print(letter, style="bold yellow", end=end)
+                elif color == "grey":
+                    self.board_printer.print(letter, style="bold #808080", end=end)
+                elif color == "green":
+                    self.board_printer.print(letter, style="bold green", end=end)
+                else:
+                    raise Exception(
+                        f"Ooops, looks like you did not pass a valid color. \
+                                     Should be: [yellow, grey, green] instead found: {color}"
+                    )
+            print()
 
-       if all(color == "green" for color,_, _ in self.past_guesses[-1]):
-           print("You win!!!")
-           sys.exit()
+        if all(color == "green" for color, _, _ in self.past_guesses[-1]):
+            print("You win!!!")
+            sys.exit()
 
-       if len(self.past_guesses) == 6:
-           print("Oops looks like you ran out of guesses. You lose :(")
-           sys.exit()
+        if len(self.past_guesses) == 6:
+            print("Oops looks like you ran out of guesses. You lose :(")
+            sys.exit()
--- play.py
+++ play.py
@@ -1,9 +1,14 @@
+from __future__ import annotations
+
+# Python Standard Library
+import csv
+
+# Custom Classes
 from game_engine import Wordle
 from game_agent import WordleAI
 
-def real_wordle_word_bank():
-    import csv
-
+def real_wordle_word_bank() -> list[str]:
+    """ read in csv of actual wordle words """
     word_bank = []
     with open("./wordle_wordlist.csv", "r") as f:
         reader = csv.reader(f)
@@ -11,53 +16,24 @@ def real_wordle_word_bank():
             word_bank.append(row[0])
     return word_bank
 
-def simple_game():
-    new_game = Wordle(answer="ART", word_bank=["ART", "CAT", "HAT", "RAT", "FOO", "BAR"])
+
+def simple_game() -> None:
+    """ initialize a simpler version of the game """
+    new_game = Wordle(
+        answer="ART", word_bank=["ART", "CAT", "HAT", "RAT", "FOO", "BAR"]
+    )
     while True:
         new_game.guess()
         new_game.render_result()
 
-def real_wordle():
+
+def real_wordle() -> None:
+    """ start a real game of wordle! """
     word_bank = real_wordle_word_bank()
     new_game = Wordle(answer=None, word_bank=word_bank)
     while True:
         new_game.guess()
-        new_game.render_result()
-
-def real_wordle_w_ai_assist():
-    word_bank = real_wordle_word_bank()
-    new_game = Wordle(answer=None, word_bank=word_bank)
-    ai = WordleAI(new_game)
-    while True:
-        use_ai = input("Get help from a.i? [y/n]: ")
-        if use_ai == "y":
-            options = ai.get_viable_guesses()
-            print(f"Given your previous guesses, there are {len(options)} potential words")
-            if len(options) < 3:
-                print("The best guesses are ", end="")
-                for idx, option in enumerate(options):
-                    if idx == len(options)-1:
-                        end = ""
-                    else:
-                        end = ", "
-                    print(f"{option}", end=end)
-                print()
-            else:
-                print(f"The best guesses are {options[0]}, {options[1]}, {options[2]}")
-
-        new_game.guess()
-        new_game.render_result()
-
-def watch_ai_play():
-    word_bank = real_wordle_word_bank()
-    new_game = Wordle(answer=None, word_bank=word_bank, ai=True)
-    ai = WordleAI(new_game)
-    while True:
-        guess = ai.random_viable_guess()
-        new_game.evaluate_guess(guess)
         new_game.render_result()
 
 if __name__ == "__main__":
     # real_wordle()
-    # real_wordle_w_ai_assist()
-    watch_ai_play()
